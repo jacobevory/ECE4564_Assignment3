5aed5f605b76b1cfaa538a16d73fe437df727461
Jacob <jacobevory@gmail.com>
2017-11-15 18:40:08 -0500

    Initial commit

---
+++ canvas.py
@@ -0,0 +1,12 @@
+canvasAccessToken = 4511~4EMizjmsMkdhFJ8db8W3NHvmHYmX5GkpT7Dl70Zk8vyAsv5TrstxRBooRA1On43s
+#Access tokens can be used to allow other applications to make API calls on your behalf. You can #also generate access tokens and *use the Canvas Open API* to come up with your own #integrations.
+
+#Token:	
+#4511~4EMizjmsMkdhFJ8db8W3NHvmHYmX5GkpT7Dl70Zk8vyAsv5TrstxRBooRA1On43s
+#Copy this token down now. Once you leave this page you won't be able to retrieve the full token #anymore, you'll have to regenerate it to get a new value.
+#App:	User-Generated
+#Purpose:	ECE4564_Assignment3
+#Created:	Nov 15 at 6:38pm
+#Last Used:	--
+#Expires:	Dec 31 at 12am
+#Regenerate Token
