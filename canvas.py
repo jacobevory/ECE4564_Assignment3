52062b8b83076ca020495e170fb7e09c323e55f2
Jacob <jacobevory@gmail.com>
2017-11-15 19:00:12 -0500

    canvas token update

--- canvas.py
+++ canvas.py
@@ -1,4 +1,4 @@
-canvasAccessToken = 4511~4EMizjmsMkdhFJ8db8W3NHvmHYmX5GkpT7Dl70Zk8vyAsv5TrstxRBooRA1On43s
+canvasAccessToken = "4511~4EMizjmsMkdhFJ8db8W3NHvmHYmX5GkpT7Dl70Zk8vyAsv5TrstxRBooRA1On43s"
 #Access tokens can be used to allow other applications to make API calls on your behalf. You can #also generate access tokens and *use the Canvas Open API* to come up with your own #integrations.
 
 #Token:	
--- services.py
+++ services.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 
+from canvas import canvasAccessToken 
+import pymongo
+from flask import Flask, requests
+
+clientIP = "127.0.0.1"
+clientPORT = 27017
+client = MongoClient(clientIP, clientPORT)
+canvas = client.canvas
+auth = client.auth
