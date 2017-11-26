a5402d0c18dc5be2d73e09f5289241b2213920c1
Rubina Adhikari <arubina@vt.edu>
2017-11-26 14:08:46 -0500

    Update zeroconf.py

--- zeroconf.py
+++ zeroconf.py
@@ -7,3 +7,22 @@ name = 'team18led'
 path = '' 
 port = 5000
 publish(name, colors, path, port)
+
+#maybe another way:
+'''
+import Zeroconf
+import socket
+ 
+server = Zeroconf.Zeroconf()
+ 
+# Get local IP address
+local_ip = socket.gethostbyname(socket.gethostname())
+local_ip = socket.inet_aton(local_ip)
+
+
+colors = {'Red', 'Yellow', 'Blue', 'Cyan', 'Green', 'White'}
+ 
+svc1 = Zeroconf.ServiceInfo('_team18led._tcp.local.', 'Colors._team18led._tcp.local.', address = local_ip, port = 2972, weight = 0, priority=0, properties = {'Colors': colors})
+server.registerService(svc1)
+
+'''
