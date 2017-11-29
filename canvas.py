f2e05475e1ee2802969a4edb2362ef47247ae3fb
MaryHailu <mary89@vt.edu>
2017-11-29 17:27:44 -0500

    Add files via upload

--- canvas.py
+++ canvas.py
@@ -1,12 +1,6 @@
-canvasAccessToken = "4511~4EMizjmsMkdhFJ8db8W3NHvmHYmX5GkpT7Dl70Zk8vyAsv5TrstxRBooRA1On43s"
-#Access tokens can be used to allow other applications to make API calls on your behalf. You can #also generate access tokens and *use the Canvas Open API* to come up with your own #integrations.
+import requests
 
-#Token:	
-#4511~4EMizjmsMkdhFJ8db8W3NHvmHYmX5GkpT7Dl70Zk8vyAsv5TrstxRBooRA1On43s
-#Copy this token down now. Once you leave this page you won't be able to retrieve the full token #anymore, you'll have to regenerate it to get a new value.
-#App:	User-Generated
-#Purpose:	ECE4564_Assignment3
-#Created:	Nov 15 at 6:38pm
-#Last Used:	--
-#Expires:	Dec 31 at 12am
-#Regenerate Token
+url ='https://vt.instructure.com/files/5541430/download?download_frd=1&verifier=6wvPx91ZgGvBNwdrAZur7R3MlkWziVRe0Yhj5rvH'
+r = requests.get(url, allow_redirects=True)
+open('file', 'wb').write(r.content)
+
