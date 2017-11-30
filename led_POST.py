5384954371c5c43fed99933772d22c8f6f861d3d
Rubina Adhikari <arubina@vt.edu>
2017-11-29 23:02:35 -0500

    Update led_POST.py

--- led_POST.py
+++ led_POST.py
@@ -34,80 +34,74 @@ server.register_service(svc1)
 
 app = Flask(__name__)
 
-@app.route('/LEDPOST', methods=['POST'])
+@app.route('/LED', methods=['POST', 'GET'])
 
-def LEDGET():
-    print('Changing LED')
-    status = str(request.get_json().get('status'))
-    print('Changing status to ' + status)
-    if status == 'off':
-        red.stop()
-        blue.stop()
-        green.stop()
-        return "LED off"
-    elif status != 'on':
-        print('Error: Status incorrect.')
-        print('Correct options are on or off.')
-        return "LED not changed due to error."
-    color = str(request.get_json().get('color'))
-    print('Changing color to ' + color)
-    if color == 'red':
-        red.start(100)
-        blue.stop()
-        green.stop()
-    elif color == 'blue':
-        red.stop()
-        blue.start(100)
-        green.stop()
-    elif color == 'yellow':
-        red.start(100)
-        blue.stop()
-        green.start(100)
-    elif color == 'magenta':
-        red.start(100)
-        blue.start(100)
-        green.stop()
-    elif color == 'cyan':
-        red.stop()
-        blue.start(100)
-        green.start(100)
-    elif color == 'green':
-        red.stop()
-        blue.stop()
-        green.start(100)
-    elif color == 'white':
-        red.start(100)
-        blue.start(100)
-        green.start(100)
-    else:
-        print('Error: Color incorrect.')
-        print('Correct options are: magenta, white, green, cyan, yellow, blue, red.')
-        return "LED not changed due to error."
+def LED():
+    if request.methods == 'POST':
+        print('Changing LED')
+        status = str(request.get_json().get('status'))
+        print('Changing status to ' + status)
+        if status == 'off':
+            red.stop()
+            blue.stop()
+            green.stop()
+            return "LED off"
+        elif status != 'on':
+            print('Error: Status incorrect.')
+            print('Correct options are on or off.')
+            return "LED not changed due to error."
+        color = str(request.get_json().get('color'))
+        print('Changing color to ' + color)
+        if color == 'red':
+            red.start(100)
+            blue.stop()
+            green.stop()
+        elif color == 'blue':
+            red.stop()
+            blue.start(100)
+            green.stop()
+        elif color == 'yellow':
+            red.start(100)
+            blue.stop()
+            green.start(100)
+        elif color == 'magenta':
+            red.start(100)
+            blue.start(100)
+            green.stop()
+        elif color == 'cyan':
+            red.stop()
+            blue.start(100)
+            green.start(100)
+        elif color == 'green':
+            red.stop()
+            blue.stop()
+            green.start(100)
+        elif color == 'white':
+            red.start(100)
+            blue.start(100)
+            green.start(100)
+        else:
+            print('Error: Color incorrect.')
+            print('Correct options are: magenta, white, green, cyan, yellow, blue, red.')
+            return "LED not changed due to error."
 
-    intensity = str(request.get_json().get('intensity'))
-    print('Changing intensity to ' + intensity)
-    inten = int(intensity)
-    if inten >= 0 & inten <= 100:
-        red.ChangeDutyCycle(inten)
-        blue.ChangeDutyCycle(inten)
-        green.ChangeDutyCycle(inten)
-    else:
-        print('Error: Intensity incorrect.')
-        print('Correct options are >= 0 and <= 100.')
-        return "LED not changed due to error."
-    return "LED changed"
-
-app.run(host='0.0.0.0', debug=True)
-
-################################################################
-@app.route('/LEDGET', methods=['GET'])
-
-def LEDPOST():    
-    newStatus = {'status': status, 'color': color}
-    print('Sent POST request with status and color')
-    return jsonify({'ledStatus': newStatus}), 201
+        intensity = str(request.get_json().get('intensity'))
+        print('Changing intensity to ' + intensity)
+        inten = int(intensity)
+        if inten >= 0 & inten <= 100:
+            red.ChangeDutyCycle(inten)
+            blue.ChangeDutyCycle(inten)
+            green.ChangeDutyCycle(inten)
+        else:
+            print('Error: Intensity incorrect.')
+            print('Correct options are >= 0 and <= 100.')
+            return "LED not changed due to error."
+        return "LED changed"
+    elif request.methods == 'GET':
+        newStatus = {'status': status, 'color': color}
+        print('Sent POST request with status and color')
+        return jsonify({'ledStatus': newStatus}), 201
 
 app.run(host='0.0.0.0', debug=True)
-################################################################
 
 GPIO.cleanup()
