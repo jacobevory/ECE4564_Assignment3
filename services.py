1c9be29ca93f389c359bb7065675f3b9d6fa0e5b
Rubina Adhikari <arubina@vt.edu>
2017-11-29 21:13:52 -0500

    Update services.py

--- services.py
+++ services.py
@@ -43,7 +43,8 @@ def on_service_state_change(zeroconf, service_type, name, state_change):
                 print("  Properties are:")
                 for key, value in info.properties.items():
                     print("    %s: %s" % (key, value))
-                    #listOfColors = value.split(' ')
+                    newList = value.decode()
+                    listOfColors = newList.split(' ')
             else:
                 print("  No properties")
         else:
