060c74abbc51c008df37400c294e1829560e676b
Rubina Adhikari <arubina@vt.edu>
2017-11-29 22:25:09 -0500

    Update zerotest.py

--- zerotest.py
+++ zerotest.py
@@ -6,13 +6,10 @@ server = zeroconf.Zeroconf()
 # Get local IP address
 local_ip = socket.gethostbyname(socket.gethostname())
 local_ip = socket.inet_aton(local_ip)
-
 svc1 = zeroconf.ServiceInfo('_team18._tcp.local.',
                               'colors._team18._tcp.local.',
                               address = local_ip,
                               port = 2972,
                               weight = 0, priority=0,
-                              properties = {'colors':
-                                            'red blue green cyan'}
-                             )
+                              properties = {'colors': 'red blue green magenta cyan yellow white'})
 server.register_service(svc1)
