cbdcbc29d9a8effc7fe4393f7d394fb6bfb7d442
Rubina Adhikari <arubina@vt.edu>
2017-11-30 22:06:22 -0500

    Update led.py

--- led.py
+++ led.py
@@ -18,8 +18,8 @@ server.register_service(svc1)
 
 time.sleep(5)
 
-svc1 = zeroconf.ServiceInfo('_team18._tcp.local.', 'colors._team18._tcp.local.', address = local_ip, port = 2972, weight = 0, priority=0, properties = {'colors': 'red blue green magenta cyan yellow white'})
-server.register_service(svc1)
+newserver = zeroconf.Zeroconf()
+newserver.register_service(svc1)
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
