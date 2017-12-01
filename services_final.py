905845a1349c1ed97cf8e0e930bf1e51cbecd440
Rubina Adhikari <arubina@vt.edu>
2017-11-30 21:18:15 -0500

    Update services_final.py

--- services_final.py
+++ services_final.py
@@ -26,37 +26,43 @@ listOfColors = []
 #LEDaddress = "172.29.18.24" 
 LEDaddress = []
 
+count = 0;
+
 def on_service_state_change(zeroconf, service_type, name, state_change):
     print("Service %s of type %s state changed: %s" % (name, service_type, state_change))
     if state_change is ServiceStateChange.Added:
         info = zeroconf.get_service_info(service_type, name)
-        if info:
+        if info:            
             #LEDaddress = info.address.decode() + ':' + info.port.decode()
             LEDaddress.append(socket.inet_ntoa(info.address))
-            print("  Address: %s:%d" % (socket.inet_ntoa(info.address), info.port))
-            print("  Weight: %d, priority: %d" % (info.weight, info.priority))
-            print("  Server: %s" % (info.server))
+            if count > 1 :
+                print("  Address: %s:%d" % (socket.inet_ntoa(info.address), info.port))
+                print("  Weight: %d, priority: %d" % (info.weight, info.priority))
+                print("  Server: %s" % (info.server))
             if info.properties:
-                print("  Properties are:")
+                if count > 1 :
+                    print("  Properties are:")
                 for key, value in info.properties.items():
                     newList = value.decode()                    
                     listOfColors.append(newList.split())
-                    print(" ", listOfColors)
+                    if count > 1 :
+                        print(" ", listOfColors)
             else:
                 print("  No properties")
         else:
             print("  No info")
         print('\n')
-
-
-zeroconf = Zeroconf()
+        
 print("\nBrowsing services\n")
-browser = ServiceBrowser(zeroconf, "_team18._tcp.local.", handlers=[on_service_state_change]) 
 
-while listOfColors == []:
-    sleep(0.1)
+while count < 1 :
+    zeroconf = Zeroconf()
+    browser = ServiceBrowser(zeroconf, "_team18._tcp.local.", handlers=[on_service_state_change])    
+    while listOfColors == []:
+        sleep(0.1)    
+    count = count + 1
+    zeroconf.close()
     
-zeroconf.close()
 
 clientIP = "127.0.0.1"
 clientPORT = 27017
