713bdcdd74fb3c331ce9e233a6f733179f3cf00c
Rubina Adhikari <arubina@vt.edu>
2017-11-30 21:05:04 -0500

    Update services_final.py

--- services_final.py
+++ services_final.py
@@ -113,7 +113,7 @@ def LED_route():
         updateColor = str(request.args.get('color')) 
         newStatus = {'status': updateStatus, 'intensity': updateIntensity, 'color': updateColor}
         print(newStatus)
-        r = requests.post("http://" + LEDaddress + "/LED", newStatus)
+        r = requests.post("http://" + LEDaddress + "/LED" + "?" + "status=" + updateStatus + "&" + "color=" + updateColor + "&" +"intensity=" + updateIntensity)
         return r.text
     
 @advertise(private=True, colors=[], methods=['GET', 'POST'])
